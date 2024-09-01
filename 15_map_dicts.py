eeb0344b955c5942dd97e8c252a3b6c409d1f301
Harold Navarro <kaendor50@gmail.com>
2024-08-31 21:15:14 -0600

    Añadiendo archivo utilizando map con listas de diccionarios

---
+++ 15_map_dicts.py
@@ -0,0 +1,40 @@
+import copy
+
+items = [
+    {
+        'product': 'camisa',
+        'price': 120
+    },
+    {
+        'product': 'pantalones',
+        'price': 300
+    },
+    {
+        'product': 'gorra',
+        'price': 50
+    }
+]
+
+# Obtener una lista con los precios de items
+################################################################
+# List comprehension
+prices = [item['price'] for item in items]
+print(prices) # => [120, 300, 50]
+
+# Map
+prices_v2 = list(map(lambda item: item['price'], items)) 
+print(prices_v2) # => [120, 300, 50]
+################################################################
+
+# Obtener una lista con los precios de items y aplicar los taxes
+################################################################
+def add_taxes(item): 
+    item = item.copy() # esto con el proposito de que no se afecta al item original y en consecuencia a la lista original, con esto creando una copia para referirnos a este item
+    item['taxes'] = item['price'] * .19
+    return item
+
+# utilizar map para transformar cada elemento de la lista con una funcion
+new_items = list(map(add_taxes, items))
+print(new_items)
+print(items)
+################################################################
