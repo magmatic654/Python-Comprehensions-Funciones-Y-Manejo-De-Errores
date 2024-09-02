ab75b50922fc373cd8e0b63b23d8d8b80e5057cc
Harold Navarro <kaendor50@gmail.com>
2024-09-01 18:59:56 -0600

    Añadiendo dualidad con modulos como scripts

---
+++ app/example.py
@@ -0,0 +1,5 @@
+import main
+# se ejecuta main aunque solo queramos imprimir la variable data
+# podemos meter todo lo ejecutable del arrchivo de origen en una funcion
+# def run():
+print('example => ', main.data)
--- app/main.py
+++ app/main.py
@@ -1,23 +1,33 @@
 import utils # importamos nuestro modulo y con ello tenemos acceso a las funciones, clases, etc dentro de el.
 
-keys, values = utils.get_population() # llamando a una funcion de nuestro modulo
-print(keys, values)
+data = [
+        {
+            'country': 'Mexico',
+            'population': '1200'
+        },
+        {
+            'country': 'Colombia',
+            'population': '300'
+        },
+        {
+            'country': 'Bolivia',
+            'population': '50'
+        }
+    ]
 
-# print(utils.A) # llamando a una variable de nuestro modulo
 
-country = input('Type Country => ').capitalize()
+def run():
+    keys, values = utils.get_population() # llamando a una funcion de nuestro modulo
+    print(keys, values)
 
-print(utils.population_by_countrie([
-    {
-        'country': 'Mexico',
-        'population': '1200'
-    },
-    {
-        'country': 'Colombia',
-        'population': '300'
-    },
-    {
-        'country': 'Bolivia',
-        'population': '50'
-    }
-], country))
+    # print(utils.A) # llamando a una variable de nuestro modulo
+
+    country = input('Type Country => ').capitalize()
+
+    result = utils.population_by_countrie(data, country)
+    print(result)
+
+# Si se ejecuta directamente, se va a ejecutar la funcion run, pero si 
+# es ejecutado desde otro archivo por importacion no va a pasar nada
+if __name__ == '__main__':
+    run()
